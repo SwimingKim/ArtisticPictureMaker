20ee1c0c932b9b14e5954fc7325227606bf16919
skim <rumex13@naver.com>
2019-08-03 15:06:10 +0900

    initialize

---
+++ app/src/main/java/com/devskim/apw/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.devskim.apw
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
